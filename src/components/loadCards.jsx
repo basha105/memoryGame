49c7086f33cd59be04bf58e3b012f23bcad4911c
basha105 <brentbasha17@gmail.com>
2024-11-27 19:30:22 -0800

    Tryin to find bug

--- src/components/loadCards.jsx
+++ src/components/loadCards.jsx
@@ -1,18 +1,28 @@
 import BuildCard from "./buildCard";
-import { useEffect } from 'react';
 import { useState } from 'react';
+import { useEffect } from "react";
 
+export default function LoadCards() {
 
-const pokemons = ['pikachu', 'squirtle', 'bulbasaur', 'charmander', 'mudkip', 'piplup', 'cyndaquil', 'munchlax', 'chimchar', 'oshawott', 'magikarp', 'meowth'];
-const [cards, setCards] = useState([]);
+    const pokemons = ['pikachu', 'squirtle', 'bulbasaur', 'charmander', 'mudkip', 'piplup', 'cyndaquil', 'munchlax', 'chimchar', 'oshawott', 'magikarp', 'meowth'];
+    const [cards, setCards] = useState([]);
+
+    // Using useEffect because the display will update as the 'cards' load
+
+    useEffect(() => {
+        async function loader() {
+            const loadedCards = await Promise.all(
+                pokemons.map((pokemon, index) => BuildCard(index))
+            );
+            setCards(loadedCards);
+        }
+        loader();
+    }, []);
     
 
     return (
-        <div>
-            {cards}
-        </div>
+        <div>{cards}</div>
     )
-
 
 
 }
