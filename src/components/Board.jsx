454e2cdcb671e113020f6c05f04cac2585372cd3
basha105 <brentbasha17@gmail.com>
2024-11-30 16:34:44 -0800

    Trying to finalize score tracking

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -6,24 +6,29 @@ import { useState } from 'react';
 export default function Board() {
     
     const [rerenderKey, setRerenderKey] = useState(false);
+    const [cardClicked, setCardClicked] = useState(false);
+
+
 
     const handleChildClick = (index) => {
         setRerenderKey(!rerenderKey);
+        setCardClicked(true); 
         console.log(`${pokemon[index]} was clicked!`)
     }
 
 
     function shuffler(array) {
         let newList = array.sort(() => Math.random() - 0.5);
-        return newList
+        return newList;
     }
     
-    let shuffledPokemon = shuffler(pokemon)
+    let shuffledPokemon = shuffler(pokemon);
     
     return (
         <div id="board" key={rerenderKey}>
             {shuffledPokemon.map((poke, index) => (
-                <Card key={index} index={index} onChildClick={() => handleChildClick(index)}/>
+                <Card key={index} index={index} onChildClick={() => handleChildClick(index)}
+                />
             ))}
         </div>
     )
